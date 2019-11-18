38e8317177c0c639481da1e66c876f5af0573326
Roman Mühlfeldner <roman@muehlfeldner.net>
2019-11-18 19:09:37 +0100

    Disable form too

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 (function() {
-  var buttons = document.getElementsByTagName("button");
+  buttons = document.getElementsByTagName("button");
 
   for (i = 0; i < buttons.length; i++) {
     cloned = buttons[i].cloneNode(true);
     cloned.onclick = false;
     buttons[i].parentNode.replaceChild(cloned, buttons[i]);
+  }
+  forms = document.getElementsByTagName("form");
+
+  for (i = 0; i < forms.length; i++) {
+    forms[i].addEventListener("submit", function(event) {
+      event.preventDefault();
+    });
   }
 })();
